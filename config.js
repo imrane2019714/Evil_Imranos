99d0375556421ca39f6c1c570d4b0076226b20d9
imrane2019714 <moussaidimrane20@outlook.com>
2026-07-26 18:10:38 +0100

    Update bot owner and configuration details

--- config.js
+++ config.js
@@ -4,11 +4,11 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['9876543210'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['212617503205'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['Evil Imranos'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'Knight Bot Mini',
+    botName: 'Evil Imranos Bot',
     prefix: ',',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
@@ -16,7 +16,7 @@ module.exports = {
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
-    packname: 'Knight Bot',
+    packname: 'Evil_Imranos Stickers,
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
